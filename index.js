9c4c97703cea9ed16a7947ebe8f1fa3d0efb5698
mursalinmirme <mursalinmir02@gmail.com>
2023-10-18 17:40:56 +0600

    I have connected carworldfleet with mongo db an create two collection and I have make the brands added system and brands get routeing system.

--- index.js
+++ index.js
@@ -4,10 +4,59 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 4100;
 
-app.get('/', async(req, res) => {
-    res.send("Hi, I am successfully placed in local server. My sub category data will comming soon...");
-})
+// middleware
+app.use(cors());
+app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@mursalin.bxh3q56.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const carsCollection = client.db('carWorldFleet').collection('carsCollectin');
+    const brandsCollection = client.db('carBrands').collection('carBrands');
+
+    app.get('/', async(req, res) => {
+        res.send("Hi, I am successfully placed in local server. My sub category data will comming soon...");
+    })
+
+    // get brands \
+    app.get('/brands', async(req, res) => {
+      const getBrands = brandsCollection.find();
+      const getBrandsResult = await getBrands.toArray();
+      res.send(getBrandsResult);
+    })
+
+    // brands add
+    app.post('/brands', async(req, res) => {
+       const getBrand = req.body;
+       const addBrandsResult = await brandsCollection.insertOne(getBrand);
+       res.send(addBrandsResult);
+       console.log(getBrand);
+       console.log('someone hitting on brands btn');
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.listen(port, () => {
