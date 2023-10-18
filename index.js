2dbf217934458eb7aa34b214f0a199c3f341e955
mursalinmirme <mursalinmir02@gmail.com>
2023-10-19 03:50:35 +0600

    I have make all product routing, get single Product routing, product update routing, system for my project

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 4100;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@mursalin.bxh3q56.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,9 +24,10 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
-    const carsCollection = client.db('carWorldFleet').collection('carsCollectin');
-    const brandsCollection = client.db('carBrands').collection('carBrands');
+    const database = client.db('carWorldFleet');
+    const carsCollection = database.collection('carsCollectin');
+    const brandsCollection = database.collection('carBrands');
+    const bannerCollection = database.collection('brandBanners');
 
     app.get('/', async(req, res) => {
         res.send("Hi, I am successfully placed in local server. My sub category data will comming soon...");
@@ -47,6 +48,104 @@ async function run() {
        console.log(getBrand);
        console.log('someone hitting on brands btn');
     })
+
+  // add products
+  app.post('/cars', async(req, res) => {
+     const getCar = req.body;
+     const addCardResult = await carsCollection.insertOne(getCar);
+     res.send(addCardResult);
+     console.log(getCar);
+     console.log('Some on hitting add products btn');
+  })
+
+  // show cars
+  app.get('/brandsCar', async(req, res) => {
+    const getCars = carsCollection.find();
+    const getCarsResult = await getCars.toArray();
+    res.send(getCarsResult);
+  })
+
+  // filter data by brand name
+  app.get('/brandsCar/:brand', async(req, res) => {
+      const paramsBrand = req.params.brand;
+      const getParamsItem = {brand: paramsBrand};
+      const getFilterCars = carsCollection.find(getParamsItem);
+      const getFilterCarsResut = await getFilterCars.toArray();
+      res.send(getFilterCarsResut);
+      console.log(paramsBrand);
+      console.log('Some on hitting to see specific brands cars');
+
+  })
+
+  // find a car data by car id
+  app.get('/brandsCar/:brand/:id', async(req, res) => {
+    const carBrand = req.params.brand;
+    const paramsId = req.params.id;
+    const findParamsItem = {_id: new ObjectId(paramsId)};
+    const findFilterCarsResut = await carsCollection.findOne(findParamsItem);
+    res.send(findFilterCarsResut);
+    console.log(carBrand);
+    console.log(paramsId);
+    console.log('Some on hitting to see single car details');
+
+})
+
+  // get all cars 
+  app.get('/cars', async(req, res) => {
+    const getCars = carsCollection.find();
+    const getCarsResult = await getCars.toArray();
+    res.send(getCarsResult);
+  })
+
+  // find a car for update details data by car id
+  app.get('/cars/:carsId', async(req, res) => {
+    const carId = req.params.carsId;
+    const findCarItem = {_id: new ObjectId(carId)};
+    const findCarResut = await carsCollection.findOne(findCarItem);
+    res.send(findCarResut);
+    console.log('Some on hitting for update a product');
+   })
+  
+  // update product 
+  app.put('/cars/:carsId', async(req, res) => {
+    const updateId = req.params.carsId;
+    const updateData = req.body;
+    const updateCarItem = {_id: new ObjectId(updateId)};
+    const options = { upsert : true };
+    const updateDoc = {
+      $set : updateData,
+    }
+    const updateCarResut = await carsCollection.updateOne(updateCarItem, updateDoc, options);
+    res.send(updateCarResut);
+    console.log('Some on hitting for update a product');
+   })
+
+   //add banners 
+   app.post('/banners', async(req, res) => {
+    const getBanner = req.body;
+    const addBannerResult = await bannerCollection.insertOne(getBanner);
+    res.send(addBannerResult);
+    console.log(getBanner);
+    console.log('Some on hitting for submit a banner');
+   })
+
+    // get banners
+    app.get('/banners', async(req, res) => {
+      const getBanners = bannerCollection.find();
+      const getBannersResult = await getBanners.toArray();
+      res.send(getBannersResult);
+    })
+    
+    // filter banners
+    app.get('/banners/:brands', async(req, res) => {
+      const brandsParams = req.params.brands;
+      const filterBanners = {brandName:brandsParams}
+      const getBanners = bannerCollection.find(filterBanners);
+      const getBannersResult = await getBanners.toArray();
+      res.send(getBannersResult);
+      console.log('some on searchng banner filter')
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
